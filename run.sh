1d1f60f58d3b92b4daaa64dc695d0864de3c5021
Sam Denty <sam@samdenty.com>
2025-01-29 22:37:43 +0000

    fix

--- run.sh
+++ run.sh
@@ -1,15 +1,7 @@
-echo Fetching asset from slow CDN, this would normally be cached forever by browser
-cat out.wasm.gz >/dev/null
-
-echo Done fetching, decompressing
-node -e "require('fs').writeFileSync('out.wasm',require('zlib').gunzipSync(require('fs').readFileSync('out.wasm.gz')))"
-
-echo Done decompressing
-
 echo printing help...
 
-wasm out.wasm -- 'supabase' 'help'
+wasm node_modules/supabase.wasm/supabase.wasm -- 'supabase' 'help'
 
 echo initializing
 
-wasm out.wasm --mapdir /repo:. -- 'supabase' 'init' '--workdir=/repo'
+wasm node_modules/supabase.wasm/supabase.wasm --mapdir /repo:. -- 'supabase' 'init' '--workdir=/repo'
